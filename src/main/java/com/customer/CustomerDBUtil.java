01c09de7d9971cc6e2c83b50277dd244ca02bc67
asirialwis <asirichathurika900@gmail.com>
2023-10-05 18:08:59 +0530

    Login system added..

---
+++ src/main/java/com/customer/CustomerDBUtil.java
@@ -0,0 +1,47 @@
+package com.customer;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import Configurations.DBConnect;
+
+public class CustomerDBUtil {
+	private static Connection con = null;
+	private static Statement stmt = null;
+	private static ResultSet rs = null;
+	
+	public static List<Customer> validate(String username , String password){
+		
+		ArrayList<Customer>cus = new ArrayList<>();
+		
+		
+		try {
+			
+			String sql = "select * from customer where username = '"+username+"' and password = '"+password+"'";
+			
+			rs = DBConnect.getDBConnection().executeQuery(sql);
+			
+			
+			if(rs.next()) {
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				String email = rs.getString(3);
+				String phone = rs.getString(4);
+				String userU = rs.getString(5);
+				String passU = rs.getString(6);
+				
+				Customer c = new Customer( id , name , email , phone , userU , passU);
+				cus.add(c);
+			}
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return cus;
+			
+		}
+}
